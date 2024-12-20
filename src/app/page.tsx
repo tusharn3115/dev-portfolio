9dde50ef3fc73c8fa93d20948830f83d7bdebcbd
Tushar Negi <negitushar81@gmail.com>
2024-12-20 09:18:48 +0530

    add Footer component with social links and styling; update global styles for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
 import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <TestimonialsSection />
       <AboutSection />
       <ContactSection />
+      <Footer />
     </div>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,37 @@
+import ArrowImg from "@/assets/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  {
+    title: "Github",
+    href: "https://github.com/tusharn3115",
+  },
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/tushar-negi-786571317/",
+  },
+  {
+    title: "X",
+    href: "https://x.com/tushxr05",
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-gradient-to-r  from-cyan-300/40 to-sky-400/40 mask-radial-center -z-10"></div>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between gap-8 items-center">
+          <div className="text-white/40">&copy; 2024. All rights reserved</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8">
+            {footerLinks.map((link) => (
+              <a href={link.href} key={link.title} className="inline-flex items-center gap-1.5">
+                <span className="font-semibold">{link.title}</span>
+                <ArrowImg className='size-4' />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
