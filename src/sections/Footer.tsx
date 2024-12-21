3226a299cfb5786ec7c191abe70db825458dc9ff
Tushar Negi <negitushar81@gmail.com>
2024-12-21 12:10:22 +0530

    complete portfolio

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -17,16 +17,22 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
-      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-gradient-to-r  from-cyan-300/40 to-sky-400/40 mask-radial-center -z-10"></div>
+    <footer className="relative overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-gradient-to-r from-cyan-300/40 to-sky-400/40 mask-radial-center -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between gap-8 items-center">
           <div className="text-white/40">&copy; 2024. All rights reserved</div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             {footerLinks.map((link) => (
-              <a href={link.href} key={link.title} className="inline-flex items-center gap-1.5">
+              <a
+                href={link.href}
+                key={link.title}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-1.5 text-white hover:text-cyan-400 transition-all duration-300 transform hover:scale-125"
+              >
                 <span className="font-semibold">{link.title}</span>
-                <ArrowImg className='size-4' />
+                <ArrowImg className="size-4 transform transition-all duration-300 hover:scale-110" />
               </a>
             ))}
           </nav>
